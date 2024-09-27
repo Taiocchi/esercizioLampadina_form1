3e496abc11c5e8d582a6116cb43cc338fb7b7dc4
Luca <152988683+Taiocchi@users.noreply.github.com>
2024-09-27 15:40:29 +0200

    Add files via upload

---
+++ esericizioLampadina_form1/Esercizio_form1/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Esercizio_form1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button2 = new Button();
+            button1 = new Button();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 15F);
+            button2.Location = new Point(183, 222);
+            button2.Name = "button2";
+            button2.Size = new Size(120, 77);
+            button2.TabIndex = 1;
+            button2.Text = "SPEGNI";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 15F);
+            button1.Location = new Point(183, 95);
+            button1.Name = "button1";
+            button1.Size = new Size(120, 77);
+            button1.TabIndex = 2;
+            button1.Text = "ACCENDI";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.accesa;
+            pictureBox1.Location = new Point(593, 121);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(73, 150);
+            pictureBox1.TabIndex = 4;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = Properties.Resources.spenta1;
+            pictureBox2.Location = new Point(593, 121);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(73, 150);
+            pictureBox2.TabIndex = 5;
+            pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ButtonFace;
+            ClientSize = new Size(800, 450);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Controls.Add(button1);
+            Controls.Add(button2);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button2;
+        private Button button1;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+    }
+}
