ee44a6cc7439335785141f63f49511b47aa6e88c
Taiocchi <luc.taio07@gmail.com>
2024-10-09 13:55:20 +0200

    Versione di prova con classe Lampadina

--- esericizioLampadina_form1/Esercizio_form1/Form1.cs
+++ esericizioLampadina_form1/Esercizio_form1/Form1.cs
@@ -2,9 +2,16 @@ namespace Esercizio_form1
 {
     public partial class Form1 : Form
     {
+        private Lampadina lampadina;
+
         public Form1()
         {
             InitializeComponent();
+            lampadina = new Lampadina();
+            lampadina.Altezza1 = pictureBox1.Top;
+            lampadina.Altezza2 = pictureBox2.Top;
+            lampadina.Stato1 = pictureBox1.Visible;
+            lampadina.Stato2 = pictureBox2.Visible;
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -14,14 +21,12 @@ namespace Esercizio_form1
 
         private void Accendi_Click(object sender, EventArgs e)
         {
-            pictureBox2.Visible = false;
-            pictureBox1.Visible = true;
+            lampadina.Accendi();
         }
 
         private void Spegni_Click(object sender, EventArgs e)
         {
-            pictureBox2.Visible = false;
-            pictureBox2.Visible = true;
+            lampadina.Spegni();
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -36,20 +41,73 @@ namespace Esercizio_form1
 
         private void Alza_Click(object sender, EventArgs e)
         {
-            if (pictureBox1.Top >= 0 && pictureBox2.Top >= 0)
-            {
-                pictureBox1.Top -= 10;
-                pictureBox2.Top -= 10;
-            }
+            lampadina.Alza();
         }
 
         private void Abbassa_Click(object sender, EventArgs e)
         {
-            if (pictureBox1.Top <= 320 && pictureBox2.Top <= 320)
+            lampadina.Abbassa();
+        }
+    }
+    public class Lampadina
+    {
+        private bool stato1;
+        private bool stato2;
+        private int altezza1;
+        private int altezza2;
+
+        public bool Stato1
+        {
+            get { return stato1; }
+            set { stato1 = value; }
+        }
+        public bool Stato2
+        {
+            get { return stato2; }
+            set { stato2 = value; }
+        }
+
+        public int Altezza1
+        {
+            get { return altezza1; }
+            set
             {
-                pictureBox1.Top += 10;
-                pictureBox2.Top += 10;
+                if (value >= 0 && value <= 320) // Supponiamo che l'altezza vada da 0 a 100
+                    altezza1 = value;
             }
+        }
+        public int Altezza2
+        {
+            get { return altezza2; }
+            set
+            {
+                if (value >= 0 && value <= 320) // Supponiamo che l'altezza vada da 0 a 100
+                    altezza2 = value;
+            }
+        }
+
+        public void Accendi()
+        {
+            Stato2 = false;
+            Stato1 = true;
+        }
+
+        public void Spegni()
+        {
+            Stato1 = false;
+            Stato2 = true;
+        }
+
+        public void Alza()
+        {
+            Altezza1 += 10;
+            Altezza2 += 10;
+        }
+
+        public void Abbassa()
+        {
+            Altezza1 -= 10;
+            Altezza2 -= 10;
         }
     }
 }
